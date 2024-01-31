0102cd8e01fc1d204a57acda1416ae44d235c74c
hadifayazi <hadifayazi4@gmail.com>
2024-01-31 16:23:28 +0100

    creating user model

--- backend/realmaisonAPI/apps/users/models.py
+++ backend/realmaisonAPI/apps/users/models.py
@@ -1,8 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 from django.core.validators import validate_email
 from django.utils.translation import gettext_lazy as _
 from django.core.exceptions import ValidationError
+from django.utils import timezone
 
 
 class CustomUserManager(BaseUserManager):
@@ -38,3 +39,35 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError(_("Superuser must have is_superuser=True."))
         return self.create_user(username, first_name, last_name, email, password, **extra_fields)
+
+
+class CustomUser(AbstractBaseUser, PermissionsMixin):
+    username = models.CharField(verbose_name=_("Username"), max_length=255, unique=True)
+    first_name = models.CharField(verbose_name=_("First Name"), max_length=50)
+    last_name = models.CharField(verbose_name=_("Last Name"), max_length=50)
+    email = models.EmailField(verbose_name=_("Email"), unique=True)
+    is_staff = models.BooleanField(verbose_name=_("Staff status"), default=False)
+    is_superuser = models.BooleanField(verbose_name=_("Superuser status"), default=False)
+    is_active = models.BooleanField(verbose_name=_("Active"), default=True)
+    date_join = models.DateTimeField(default=timezone.now, verbose_name=_("Date Joined"))
+
+    USERNAME_FIELD = 'email'
+    EMAIL_FIELD = 'email'
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    objects = CustomUserManager()
+
+    class Meta:
+        verbose_name = _("User")
+        verbose_name_plural = _("Users")
+
+    @property
+    def get_full_name(self):
+        return f"{self.first_name.title()} {self.last_name.title()}"
+
+    @property
+    def get_short_name(self):
+        return self.first
+
+    def __str__(self):
+        return self.username
