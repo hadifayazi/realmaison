7a89c167575ff75b02ced0c1e0896827486807e1
hadifayazi <hadifayazi4@gmail.com>
2024-01-31 15:38:56 +0100

    creating custom user manager

--- backend/realmaisonAPI/apps/users/models.py
+++ backend/realmaisonAPI/apps/users/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.core.validators import validate_email
+from django.utils.translation import gettext_lazy as _
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+
+class CustomUserManager(BaseUserManager):
+    def email_validator(self, email):
+        try:
+            validate_email(email)
+        except ValidationError:
+            raise ValidationError({"email": _("Enter a valid email address.")})
+
+    def create_user(self, username, first_name, last_name, email, password, **extra_fields):
+        if not username:
+            raise ValueError(_("Users must have an username"))
+        if not first_name and not last_name:
+            raise ValueError(_("Users must have a first and last name"))
+        if not email:
+            raise ValueError(_("Users must have an email address"))
+        if email:
+            self.email_validator(self.normalize_email(email))
+
+        user = self.model(username=username, first_name=first_name, last_name=last_name, email=email, **extra_fields)
+        user.set_password(password)
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, first_name, last_name, email, password, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+        return self.create_user(username, first_name, last_name, email, password, **extra_fields)
