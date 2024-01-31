0db4fbd886b352956bec6b1cdee64b278ed86e6c
hadifayazi <hadifayazi4@gmail.com>
2024-01-31 20:34:43 +0100

    registering profile

--- backend/realmaisonAPI/apps/profiles/admin.py
+++ backend/realmaisonAPI/apps/profiles/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+
+class ProfilesAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", 'gender', 'phone_number', 'country', 'city')
+    list_filter = ('gender', 'city')
+    list_display_links = ('id', 'user')
+
+
+admin.site.register(Profile, ProfilesAdmin)
