dc833b0078e24ceec32514b723c89a5e5b7e6684
hadifayazi <hadifayazi4@gmail.com>
2024-01-31 23:52:28 +0100

    updating user serializer

--- backend/realmaisonAPI/apps/users/serializers.py
+++ backend/realmaisonAPI/apps/users/serializers.py
@@ -1,9 +1,39 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.utils.translation import gettext_lazy as _
+from phonenumber_field.formfields import PhoneNumberField
+from django_countries.fields import CountryField
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
+    gender = serializers.CharField(source="profile.gender")
+    phone_number = serializers.CharField(source="profile.phone_number")
+    prifile_photo = serializers.ImageField(source="profile.prifile_photo")
+    first_name = serializers.SerializerMethodField()
+    last_name = serializers.SerializerMethodField()
+    full_name = serializers.SerializerMethodField(source="get_full_name")
+    country = serializers.CharField(source="profile.country")
+    city = serializers.CharField(source="profile.city")
+
+    class Meta:
+        model: CustomUser
+        fields = ('id', 'username', 'first_name', 'last_name', 'email', 'gender',
+                  'phone_number', 'prifile_photo',  'country', 'city', 'full_name')
+
+    def get_fist_name(self, obj: CustomUser):
+        return obj.first_name
+
+    def get_last_name(self, obj: CustomUser):
+        return obj.last_name
+
+    def to_representation(self, instance):
+        representation = super(UserSerializer).to_representation(instance)
+        if instance.is_superuser:
+            representation['Admin'] = True
+        return representation
+
+
+class CustomCreateUserSerializer(serializers.ModelSerializer):
     password2 = serializers.CharField(style={'input_type': 'password'})
 
     class Meta:
