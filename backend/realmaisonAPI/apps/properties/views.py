ee1066b4a5a273533878b0b2b9bc897b10d96277
hadifayazi <hadifayazi4@gmail.com>
2024-02-01 18:30:05 +0100

    creating filters for property models

--- backend/realmaisonAPI/apps/properties/views.py
+++ backend/realmaisonAPI/apps/properties/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+import logging
+import django_filters
 
-# Create your views here.
+from .models import Property
+from .pagination import PropertyPagination
+from .serializers import PropertySerializer
+
+from rest_framework import filters, generics, permissions, status
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
+
+
+logger = logging.getLogger(__name__)
+
+
+class PropertyFilter(django_filters.FilterSet):
+    house_type = django_filters.CharFilter(
+        field_name="house_type", lookup_expr='iexact')
+
+    sale_type = django_filters.CharFilter(
+        field_name="sale_type", lookup_expr='iexact')
+
+    price = django_filters.NumberFilter()
+    price__gte = django_filters.NumberFilter(field_name="price", lookup_expr="gte")
+    price__lte = django_filters.NumberFilter(field_name="price", lookup_expr="lte")
+
+    city = django_filters.CharFilter(field_name="city", lookup_expr="iexact")
+    country = django_filters.CharFilter(field_name="country", lookup_expr="iexact")
+
+    class Meta:
+        model = Property
+        fields = ['house_type', 'sale_type', 'price', 'city', 'country']
