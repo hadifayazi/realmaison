39217a5b3d3927cd5efe243cf8cdc29d7239007f
hadifayazi <hadifayazi4@gmail.com>
2024-02-01 16:55:49 +0100

    creating enquiries model and serializer

--- backend/realmaisonAPI/apps/enquiries/admin.py
+++ backend/realmaisonAPI/apps/enquiries/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Enquiry
 
-# Register your models here.
+
+class EnquiriesAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", 'phone_number', 'email', 'subject', 'message')
+
+
+admin.site.register(Enquiry, EnquiriesAdmin)
