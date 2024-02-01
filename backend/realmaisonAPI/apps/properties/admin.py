bef5dd0ef042e025f8be6f04e3584112a7b00406
hadifayazi <hadifayazi4@gmail.com>
2024-02-01 16:57:42 +0100

    registering  model

--- backend/realmaisonAPI/apps/properties/admin.py
+++ backend/realmaisonAPI/apps/properties/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Property
 
-# Register your models here.
+
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ('title', 'country', 'city',  'price',
+                    'bedrooms', 'bathrooms', 'house_type', 'surface', 'sale_type')
+    list_filter = ('country', 'city', 'sale_type', 'is_published', 'price')
+
+
+admin.site.register(Property, PropertyAdmin)
