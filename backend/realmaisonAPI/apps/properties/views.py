0a18f171299798a0f2a2f990b527e7fd4b14e120
hadifayazi <hadifayazi4@gmail.com>
2024-02-05 23:22:26 +0100

    PropertyCreate view

--- backend/realmaisonAPI/apps/properties/views.py
+++ backend/realmaisonAPI/apps/properties/views.py
@@ -101,3 +101,17 @@ class PropertyUpdate(generics.UpdateAPIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class PropertyCreate(generics.CreateAPIView):
+    permission_classes = (permissions.IsAuthenticated,)
+    serializer_class = PropertySerializer
+
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        serializer = PropertySerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save(user=user)
+            logger.info(
+                f"User {user.username}, created property: ${serializer.data['title']}, reference: {serializer.data.get('reference')}")
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
