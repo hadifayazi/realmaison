3445f8d395f3a528ae6fceb05a7b3a14781e0fd6
hadifayazi <hadifayazi4@gmail.com>
2024-02-06 01:34:19 +0100

    adding routing property app

--- backend/realmaisonAPI/apps/properties/urls.py
+++ backend/realmaisonAPI/apps/properties/urls.py
@@ -2,5 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.AllPropertiesList.as_view(), name='all_properties_list'),
+    path('', views.AllPropertiesList.as_view(), name='all-properties-list'),
+    path('realtor', views.RealtorPropertiesList.as_view(), name="all-realtor-properties"),
+    path('create', views.PropertyCreate.as_view(), name='create-property'),
+    path('details/<slug:slug>', views.PropertyViewsDetail.as_view(), name="property-details"),
+    path('update/<slug:slug>', views.PropertyUpdate.as_view(), name="update-property"),
+    path('delete/<slug:slug>', views.DeleteProperty.as_view(), name="delete-property"),
+    path('photos/upload/<slug:slug>', views.PropertyPhotosUploadView.as_view(), name="upload-photos"),
+    path('search', views.PropertySearchView.as_view(), name="search-properties"),
+
 ]
