bc730be65d5204c2f67bc8127e978d2f679ca756
hadifayazi <hadifayazi4@gmail.com>
2024-02-07 20:32:50 +0100

    creating a api call to get properties belonging to a realtor

--- client/src/store/apis/properties.js
+++ client/src/store/apis/properties.js
@@ -4,14 +4,15 @@ export const propertiesApi = createApi({
   reducerPath: "propertiesApi",
   baseQuery: fetchBaseQuery({
     baseUrl: "http://127.0.0.1:8000/api/v1/properties/",
-    // prepareHeaders: (headers) => {
-    //   headers.set("Access-Control-Allow-Origin", "true");
-    //   headers.set("Content-Type", "application/json");
-    //   headers.set("Accept", "application/json");
-    //   headers.set("mode", "cors");
+    prepareHeaders: (headers) => {
+      headers.append(
+        "Authorization",
+        "Bearer " +
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzA3MzM2MTY5LCJpYXQiOjE3MDczMjg5NjksImp0aSI6IjJmYTQ3MzcyM2NhZTRlMTA5YTI1ZmE1YTk5NmMzNWIyIiwidXNlcl9pZCI6OH0.fD4qGjontct3snUbWiEV_0Wt2FjTNF1qaI5RQwPrMt8"
+      );
 
-    //   return headers;
-    // },
+      return headers;
+    },
   }),
   endpoints: (builder) => ({
     getProperties: builder.query({
@@ -23,7 +24,17 @@ export const propertiesApi = createApi({
         };
       },
     }),
+    getRealtorProperties: builder.query({
+      query: () => {
+        return {
+          url: "realtor",
+          method: "GET",
+          credentials: "include",
+        };
+      },
+    }),
   }),
 });
 
-export const { useGetPropertiesQuery } = propertiesApi;
+export const { useGetPropertiesQuery, useGetRealtorPropertiesQuery } =
+  propertiesApi;
