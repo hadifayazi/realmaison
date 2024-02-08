03c880bd639fdb1bdd0b8630ce6e441547e6115f
hadifayazi <hadifayazi4@gmail.com>
2024-02-08 03:29:00 +0100

    creating router

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import RootLayout from "./hocs/RootLayout";
+import PropertyList from "./components/PropertyList";
+import AboutUs from "./components/AboutUs";
+import PropertyDetails from "./components/PropertyDetails";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<PropertyList />} />,
+      <Route path="/about" element={<AboutUs />} />,
+      <Route path="/details/:slug" element={<PropertyDetails />} />,
+      {/* <Route path="*" element={<NotFound />} /> */}
+    </Route>
+  )
+);
 
 const App = () => {
-  return (
-    <div>
-      <RootLayout>
-        <div>this is</div>
-      </RootLayout>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 };
 
 export default App;
