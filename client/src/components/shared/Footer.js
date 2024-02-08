34ac2f74eaf65a805f75f1891386557bc0a8f474
hadifayazi <hadifayazi4@gmail.com>
2024-02-08 01:13:06 +0100

    base footer

--- client/src/components/shared/Footer.js
+++ client/src/components/shared/Footer.js
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div
+      style={{
+        backgroundColor: "#FFDBA4",
+        display: "flex",
+        gap: "10px",
+        justifyContent: "space-around",
+        color: "#607274",
+      }}
+    >
+      <div style={{ display: "flex", flexDirection: "column" }}>
+        <div>Contact us?</div>
+        <div>Copywrite</div>
+      </div>
+      <div>It's just a footer get over it!</div>
+    </div>
+  );
 };
 
 export default Footer;
