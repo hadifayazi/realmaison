c0193ac815afe74f6496a25314c7ded36a993a85
hadifayazi <hadifayazi4@gmail.com>
2024-02-08 01:12:51 +0100

    creating header

--- client/src/components/shared/Header.js
+++ client/src/components/shared/Header.js
@@ -1,7 +1,26 @@
-import React from "react";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
-const Header = () => {
-  return <div>Header</div>;
-};
+function Header() {
+  return (
+    <>
+      <Navbar style={{ backgroundColor: "#FFB3B3" }} variant="dark" expand="md">
+        <Container>
+          <Navbar.Brand href="#home">RealMaison</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbar-nav" />
+          <Navbar.Collapse id="navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#home">Properties</Nav.Link>
+              <Nav.Link href="#features">Realtors</Nav.Link>
+              <Nav.Link href="#pricing">About us</Nav.Link>
+              <Nav.Link href="#pricing">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+}
 
 export default Header;
