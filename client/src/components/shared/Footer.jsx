09bfa5482e63f06dce22ce9071baaeeb6b8653d6
hadifayazi <hadifayazi4@gmail.com>
2024-02-14 22:46:12 +0100

    header footer modif

--- client/src/components/shared/Footer.jsx
+++ client/src/components/shared/Footer.jsx
@@ -1,22 +1,16 @@
-import React from "react";
+import { Container, Col, Row } from "react-bootstrap";
 
 const Footer = () => {
   return (
-    <div
-      style={{
-        backgroundColor: "#FFDBA4",
-        display: "flex",
-        gap: "10px",
-        justifyContent: "space-around",
-        color: "#607274",
-      }}
-    >
-      <div style={{ display: "flex", flexDirection: "column" }}>
-        <div>Contact us?</div>
-        <div>Copywrite</div>
-      </div>
-      <div>It's just a footer get over it!</div>
-    </div>
+    <footer style={{ backgroundColor: "black", color: "white" }}>
+      <Container>
+        <Row>
+          <Col>
+            <p>Copyright &copy; RealMaison {new Date().getFullYear()} </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
   );
 };
 
--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,21 +1,22 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <>
-      <Navbar style={{ backgroundColor: "#FFB3B3" }} variant="dark" expand="md">
+      <Navbar style={{ backgroundColor: "#000000" }} variant="dark" expand="md">
         <Container>
-          <Navbar.Brand href="#home">RealMaison</Navbar.Brand>
+          <Navbar.Brand className="logo" href="/">
+            Real<span style={{ color: "#da0c81" }}>M</span>aison
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbar-nav" />
           <Navbar.Collapse id="navbar-nav">
             <Nav className="me-auto">
-              <Link to="/">Properties</Link>
-              <Nav.Link href="#features">Realtors</Nav.Link>
-              <Link to="/about">About us</Link>
-              <Nav.Link href="#pricing">Contact</Nav.Link>
+              <Nav.Link href="/properties">Properties</Nav.Link>
+              <Nav.Link href="/realtors">Realtors</Nav.Link>
+              <Nav.Link href="/about">About us</Nav.Link>
+              <Nav.Link href="/contact">Contact</Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
