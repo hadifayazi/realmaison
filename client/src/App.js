5634e398ec898968a558e5738692d56a03367248
hadifayazi <hadifayazi4@gmail.com>
2024-02-16 15:04:18 +0100

    fixing refresh on each click by removoinf extra comma

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,20 @@ import {
   RouterProvider,
 } from "react-router-dom";
 import RootLayout from "./hocs/RootLayout";
-import PropertyList from "./components/PropertyList";
+import PropertiesPage from "./pages/PropertiesPage";
 import AboutUs from "./components/AboutUs";
 import PropertyDetails from "./components/PropertyDetails";
+import HomePage from "./pages/HomePage";
+import NotFound from "./pages/NotFound";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<RootLayout />}>
-      <Route index element={<PropertyList />} />,
-      <Route path="/about" element={<AboutUs />} />,
-      <Route path="/details/:slug" element={<PropertyDetails />} />,
-      {/* <Route path="*" element={<NotFound />} /> */}
+      <Route index element={<HomePage />} />
+      <Route path="/properties" element={<PropertiesPage />} />
+      <Route path="/about" element={<AboutUs />} />
+      <Route path="/details/:slug" element={<PropertyDetails />} />
+      <Route path="*" element={<NotFound />} />
     </Route>
   )
 );
