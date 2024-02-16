a2716656c0e0fb64f6e311bb7171f8c876536a09
hadifayazi <hadifayazi4@gmail.com>
2024-02-16 15:02:22 +0100

    header: using link instead of nav.link

--- client/src/components/shared/Header.jsx
+++ client/src/components/shared/Header.jsx
@@ -1,22 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
+import { Link } from "react-router-dom"; // Import Link from react-router-dom
 
 function Header() {
   return (
     <>
       <Navbar style={{ backgroundColor: "#000000" }} variant="dark" expand="md">
         <Container>
-          <Navbar.Brand className="logo" href="/">
+          <Link to="/" className="logo" style={{ color: "white" }}>
             Real<span style={{ color: "#da0c81" }}>M</span>aison
-          </Navbar.Brand>
+          </Link>
           <Navbar.Toggle aria-controls="navbar-nav" />
           <Navbar.Collapse id="navbar-nav">
             <Nav className="me-auto">
-              <Nav.Link href="/properties">Properties</Nav.Link>
-              <Nav.Link href="/realtors">Realtors</Nav.Link>
-              <Nav.Link href="/about">About us</Nav.Link>
-              <Nav.Link href="/contact">Contact</Nav.Link>
+              <Link to="/properties" className="nav-link">
+                Properties
+              </Link>
+              <Link to="/realtors" className="nav-link">
+                Realtors
+              </Link>
+              <Link to="/about" className="nav-link">
+                About us
+              </Link>
+              <Link to="/contact" className="nav-link">
+                Contact
+              </Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
